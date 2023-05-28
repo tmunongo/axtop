f841594d78e50e1c18c11bc995e434b8a79b4516
Tawanda Munongo <ejmunongo@gmail.com>
2023-05-28 15:24:07 +0200

    cpu return as json

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::sync::{Arc, Mutex};
 
-use axum::{Router, Server, routing::get, extract::State};
+use axum::{Router, Server, routing::get, extract::State, Json, response::IntoResponse};
 use sysinfo::{System, SystemExt, CpuExt};
 
 #[tokio::main]
@@ -8,6 +8,7 @@ use sysinfo::{System, SystemExt, CpuExt};
 async fn main() {
     let router = Router::new()
     .route("/", get(root_get))
+    .route("/api/cpus", get(cpus_get))
     .with_state(AppState {
         sys: Arc::new(Mutex::new(System::new())),
     });
@@ -25,21 +26,23 @@ struct AppState {
     sys: Arc<Mutex<System>>,
 }
 
-async fn root_get(State(state): State<AppState>) -> String {
-    use std::fmt::Write;
+async fn root_get() -> &'static str {
+   "Welcome to Axtop"
+}
 
-    let mut s = String::new();
-    
+async fn cpus_get(State(state): State<AppState>) -> impl IntoResponse {
     let mut sys = state.sys.lock().unwrap();
-    
-    sys.refresh_cpu();
-    
-    for (i, cpu) in sys.cpus().iter().enumerate() {
-        let i = i + 1;
-        let usage = cpu.cpu_usage();
-        
-        writeln!(&mut s,"CPU {i} {usage}%").unwrap();
-    }
 
-    s
+    sys.refresh_cpu();
+
+    let v: Vec<_> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
+    
+    // for (i, cpu) in sys.cpus().iter().enumerate() {
+    //     let i = i + 1;
+    //     let usage = cpu.cpu_usage();
+        
+    //     writeln!(&mut s,"CPU {i} {usage}%").unwrap();
+    // }
+
+    Json(v)
 }
